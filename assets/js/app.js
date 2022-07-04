e0bde668b2ed35e12c25f1dc9f394614b2b98058
denisse Rosental <denisserosentalc@gmail.com>
2022-07-04 19:21:36 -0400

    variables html

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,10 +6,23 @@ let container = document.querySelector('.container')
 
 //crear clase item que contendra en su constructor un metodo llamado crear div
 class item {
+    constructor(inputPricipal) {
+        this.inputPrincipal = inputPricipal
+    }
 
 };
 // crear un elemento input dentro de un metodo llamado crear div
-crearDiv() {
-    let inputItem = document.createElement('input')
-    inputItem.setAttribute = ("type", "text")
-};
+crearDiv(tarea) {
+    //Crear elemento input
+    let inputItem = document.createElement("input");
+    inputItem.setAttribute("type", "text");
+    inputItem.setAttribute("disabled", true);
+    inputItem.classList.add("item-input");
+    inputItem.value = tarea;
+
+    //crear div
+    let nuevoDiv = document.createElement("div");
+    nuevoDiv.classList.add("item")
+
+
+}
