52bcfa6e9c0516d2bccbdf6d17d1f7ec9cd37427
denisse Rosental <denisserosentalc@gmail.com>
2022-07-04 18:08:03 -0400

    creación de método y estructura general

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+//variables html
+
+let input = document.querySelector('.input')
+let boton = document.querySelector('.boton-agregar')
+let container = document.querySelector('.container')
+
+//crear clase item que contendra en su constructor un metodo llamado crear div
+class item {
+
+};
+// crear un elemento input dentro de un metodo llamado crear div
+crearDiv() {
+    let inputItem = document.createElement('input')
+    inputItem.setAttribute = ("type", "text")
+};
